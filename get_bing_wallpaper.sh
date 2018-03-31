2643e6de8d41b25890bf2d091291cb42079d6a06
Pratik Joshi <pratik.joshi@appdynamics.com>
2018-03-30 23:11:10 -0700

    Adding scripts

---
+++ get_bing_wallpaper.sh
@@ -0,0 +1,6 @@
+# This script is to get today's Bing wallpaper and put it in the directory from which Mac pulls it up
+
+set +x
+cd /Users/pratik.joshi/Downloads/bing-wallpaper
+./bing-wallpaper.sh -p ~/Documents/Wallpapers/
+
